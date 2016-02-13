234f33eb3984296bfdce4f2a2b92060a8b8935e0
Matthewacon <matthewjbarichello@gmail.com>
2016-02-12 22:34:59 -0500

    Multiple fixes / changes  - Fixed pacakge localization in org.usfirst.frc.team4716.robot.commands.Climber/face.java  - Fixed JSON Parsing in org.usfirst.frc.team.4716.robot.utils/ConfigClient.java  - Bumped up .gitnore file to the repo base dir and updated file  - Prepped the Raider folder containing the project src to be bumped up to the base dir of the project  - Prepped the Raider_Server to be moved to it's own project and bumped up to it's respective base dir for the project  - Prepped Raider project for src dir movement to shorten package names (Ex. /src/robot.commands <- under org.usfirst.frc.team4716)

--- Raider/src/org/usfirst/frc/team4716/robot/commands/Climber/face.java
+++ Raider/src/org/usfirst/frc/team4716/robot/commands/Climber/face.java
@@ -1,4 +1,4 @@
-package org.usfirst.frc.team4716.robot.commands;
+package org.usfirst.frc.team4716.robot.commands.Climber;
 
 import edu.wpi.first.wpilibj.command.Command;
 
--- Raider/src/org/usfirst/frc/team4716/robot/utils/ConfigClient.java
+++ Raider/src/org/usfirst/frc/team4716/robot/utils/ConfigClient.java
@@ -1,11 +1,17 @@
 package org.usfirst.frc.team4716.robot.utils;
 
-import org.json.*;
-import org.usfirst.frc.team4716.robot.RobotMap;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringReader;
 import java.net.Socket;
-import java.util.Scanner;;
+import java.util.Scanner;
+
+import org.json.simple.parser.ContainerFactory;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.usfirst.frc.team4716.robot.RobotMap;
+
+import com.sun.corba.se.impl.io.TypeMismatchException;
 
 public class ConfigClient {
 	
@@ -46,16 +52,46 @@ public class ConfigClient {
 		// TODO parse everything 
 	}
 	
+	@SuppressWarnings("static-access")
 	private static void parseRuntimeResult(){
 		// TODO only parse variable such as speed as rate 
-		
+
+		/**Idk who's code this was, but it was way off...
 		JSONObject root = new JSONObject(ConfigClient.configJsonText);
 		//RobotMap.TEST_SPEED = root.getDouble("speed");
 		System.out.println("OEHHEHEHEHEH : " + root.getDouble("speed"));
+		
 		// just trying to mess with gyro port for experiment 
 		//RobotMap.gyroPort = root.getJSONObject("sensor_configs")
 		//						.getJSONObject("gyro")
 		//						.getInt("port");
+		 */
+		
+		/**Fixed your JSON Parsing, whoever wrote that. Just use the value of testSpeed
+		 * for whatever it was that you were doing.
+		 * @author Matthewacon
+		 */
+		JSONParser jsnpars = new JSONParser();
+		double testSpeed = 0;
+		try {
+			Object o = jsnpars.parse(new StringReader(configJsonText), (ContainerFactory)null);
+			if (!(o instanceof Double)) {
+				throw new TypeMismatchException();
+			} else {
+				testSpeed = (Double)o;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TypeMismatchException e) {
+			e.printStackTrace();
+		}
+		//Some deprecated value from the last broken code block?
+//		RobotMap.TEST_SPEED = testSpeed;
+		
 	}
 	
 	private static boolean queryServer() {
