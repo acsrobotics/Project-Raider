0dc69f02c59b095bbb42caaba5e8e55c9e5a4caf
GoldenTeeth <archer.xn@live.cn>
2016-02-22 16:01:50 -0500

    Merged with fixing_climber

---
+++ Raider/src/org/usfirst/frc/team4716/robot/commands/Auto/Shoot.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team4716.robot.commands.Auto;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class Shoot extends CommandGroup {
+    
+    public  Shoot() {
+    	
+    }
+}
